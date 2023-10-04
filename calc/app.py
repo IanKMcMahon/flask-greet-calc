84e4acd1ce6081a465883dc6ff8656688ff85aff
Your Name <you@example.com>
2023-10-03 19:41:48 -0700

    complete calc app.py

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,28 @@
-# Put your app in here.
+
+from flask import Flask
+
+app = Flask(__calcapp__)
+
+
+@app.route('/add')
+def add(a, b):
+    """Returns a + b"""
+    return a + b
+
+
+@app.route('/sub')
+def subtract(a, b):
+    """Returns a - b"""
+    return a - b
+
+
+@app.route('/mult')
+def multiply(a, b):
+    """Returns a * b"""
+    return a * b
+
+
+@app.route('/div')
+def divide(a, b):
+    """Returns a / b"""
+    return a / b
